22793f28b12625d003b425b5c57c9134a947e7ed
hhacker1999 <harsh.sadh313@gmail.com>
2021-05-23 07:32:45 +0530

    Refactor again

--- lib/src/types.dart
+++ lib/src/types.dart
@@ -2,4 +2,4 @@ import 'package:flutter/widgets.dart';
 
 typedef ModelBuilderWidget<S> = Widget Function(BuildContext context, S state);
 
-typedef ModelListenerWidget<S> = Function(BuildContext context, S state);
+typedef ModelListenerWidget<S> = void Function(BuildContext context, S state);
